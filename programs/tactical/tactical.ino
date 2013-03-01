d494eb7760cb4fb5e002c6aaf57b899b456e4f9c
klxr42 <klxr42@gmail.com>
2013-02-28 16:01:13 -0800

    Update tactical.ino
    Just playing around with the code

--- programs/tactical/tactical.ino
+++ programs/tactical/tactical.ino
@@ -35,30 +35,23 @@ hexbright hb;
 #define HOLD_TIME 250 // milliseconds before going to strobe
 
 #define BRIGHTNESS_COUNT 4
-int brightness[BRIGHTNESS_COUNT] = {1000, 600, 300, OFF_LEVEL};
+int brightness[BRIGHTNESS_COUNT] = {25, 300, 600, OFF_LEVEL};
 int current_brightness = BRIGHTNESS_COUNT-1; // start on the last mode (off)
 
 void setup() {
-  hb.init_hardware(); 
+  hb.init_hardware();
 }
 
 void loop() {
   hb.update();
   
-  if(hb.button_just_released() && hb.button_pressed_time()<HOLD_TIME) { 
+  if(hb.button_just_released() && hb.button_pressed_time()<HOLD_TIME) {
     // if held for less than 200 ms before release, change regular modes
     current_brightness = (current_brightness+1)%BRIGHTNESS_COUNT;
     hb.set_light(CURRENT_LEVEL, brightness[current_brightness], 50);
   } else if (hb.button_pressed_time()>HOLD_TIME) {
     if(hb.button_pressed()) {
-      // held for over HOLD_TIME ms, go to strobe
-      static unsigned long flash_time = millis();
-      if(flash_time+70<millis()) { // flash every 70 milliseconds
-        flash_time = millis(); // reset flash_time
-        hb.set_light(MAX_LEVEL, 0, 20); // and pulse (going from max to min over 20 milliseconds)
-        // actually, because of the refresh rate, it's more like 'go from max brightness on high
-        //  to max brightness on low to off.
-      }
+      hb.set_light(CURRENT_LEVEL, 1000, 50);
     } else { // we have been doing strobe, but the light is now released
       // after strobe, go to previous light level:
       hb.set_light(CURRENT_LEVEL, brightness[current_brightness], 50);
@@ -66,4 +59,3 @@ void loop() {
   }
   hb.print_charge(GLED);
 }
-
